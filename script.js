a1339f69b9b51097ceafc8e4f8252c90c6528cad
Kyle Desktop <kylespresser@gmail.com>
2019-01-09 23:00:27 -0500

    lecture 6

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 window.onload = function() {
-  var label = 'Function is called';
-  console.count(label);
-  console.count(label);
-
-  function foo() {
-    console.count(label);
-  }
-
-  foo();
-  foo();
-  foo();
-  foo();
-  foo();
+  console.dirxml(document.body);
 };
