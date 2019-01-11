81f992cda78886e4cefdaa51cc5585b8fc1acd3d
Kyle Desktop <kylespresser@gmail.com>
2019-01-10 21:17:03 -0500

    lecture 8

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 window.onload = function() {
   $.ajax({
-    url: 'test.url.co.uk',
+    url: 'https://regres.in/api/users',
     type: 'GET',
-    error: function(errorResponse) {
-      console.error(errorResponse.statusText);
+    success: function(response) {
+      var users = response.data;
+
+      for (var i = 0; i < users.length; i++) {
+        console.group('User');
+        console.log(users[i].id);
+        console.group('Name');
+        console.log(users[i].first_name);
+        console.log(users[i].last_name);
+        console.groupEnd();
+        console.log(users[i].avatar);
+        console.groupEnd();
+      }
     }
   });
 };
